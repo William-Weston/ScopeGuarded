c021376541add39d566468a96c30acc128e7129b
William Weston <wjtWeston@protonmail.com>
2024-06-06 16:03:20 -0400

    Add test file for ScopeGuard.h

---
+++ src/src/ScopeGuard.t.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file   ScopeGuard.t.cpp
+ * @author William Weston (wjtWeston@protonmail.com)
+ * @brief  Test file for ScopeGuard.hpp
+ * @version 0.1
+ * @date    2024-06-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "ScopeGuard/ScopeGuard.hpp"
+
+#include <catch2/catch_all.hpp>
+
+#include <iostream>
+
+#define STRINGIFY_IMPL( str ) #str
+#define STRINGIFY( str ) STRINGIFY_IMPL( str )
+
+TEST_CASE( "ScopeGuard.Basic" )
+{
+   using namespace ScopeGuarded;
+
+   auto check = 0;
+   auto fn = [&]{ check = 23;  };
+   {
+      auto sg  = ScopeGuard( fn );
+   }
+
+   REQUIRE( check == 23 );
+
+   std::cerr << STRINGIFY( SCOPE_EXIT ) << '\n';
+   std::cerr << STRINGIFY( SCOPE_EXIT ) << '\n';
+
+   SCOPE_EXIT
+   {
+      std::cerr << "\nOn Exit\n";
+   };
+}
